90aa31b5fcd633027bc256e2ab06a3f7b32721bd
20135336 <20135336@tafe.wa.edu.au>
2024-12-03 21:53:33 +0800

    feat(Sensor&Display): Add attributes, constructor and str methods for display and sensor classes

--- src/display.py
+++ src/display.py
@@ -1,2 +1,9 @@
 class Display:
-    ...
+    def __init__(self, id, car_park, message=" ", is_on="false"):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"Display {self.id}: {self.message}."
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,15 @@
 class Sensor:
+    def __init__(self, id, car_park, is_active=False):
+        self.id = id
+        self.car_park = car_park
+        self.is_active = is_active
+
+    def __str__(self):
+        return f'{self.id}: {self.is_active}'
+
+
+class EntrySensor(Sensor):
+    ...
+
+class ExitSensor(Sensor):
     ...
