99d42a10544490e9217ce1ab131fb2c636df6ee5
20135336 <20135336@tafe.wa.edu.au>
2024-12-03 23:26:33 +0800

    feat(Display): Add update method

--- src/display.py
+++ src/display.py
@@ -5,5 +5,9 @@ class Display:
         self.is_on = is_on
         self.car_park = car_park
 
+    def update(self, data):
+        for key, value in data.items():
+            print(f"{key}: {value}")
+
     def __str__(self):
         return f"Display {self.id}: {self.message}."
