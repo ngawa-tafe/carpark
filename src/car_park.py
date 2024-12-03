1d0b649337ecfaa76c9c92b3d350135f615c3499
20135336 <20135336@tafe.wa.edu.au>
2024-12-03 21:13:45 +0800

    feat(carpark): Add attributes, constructor and str method

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,10 @@
 class CarPark:
-    ...
+    def __init__(self, location='unknown', capacity=100, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.displays = displays or []
+
+    def __str__(self):
+        return f"Location: {self.location}, Capacity: {self.capacity}"
